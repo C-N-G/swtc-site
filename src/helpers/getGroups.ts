b70ef9f8639dbd25560a85e5f4288bf4acf4f91c
Cormac <cormac_gudge@hotmail.com>
2024-08-27 11:39:32 +0100

    Update grouping logic for new structure

--- src/helpers/getGroups.ts
+++ src/helpers/getGroups.ts
@@ -1,7 +1,33 @@
+import fs from 'node:fs/promises';
 import { glob } from 'glob';
+import fm from "front-matter";
+import type { FrontMatterResult } from 'front-matter';
 import path from 'node:path';
 
-const mdfiles = await glob('./vault/**/*.md',);
+/**
+ * Gets the front matter from a specified file and checks if it should be published or not
+ * @param path
+ * @returns boolean flag if publish is true or false
+ */
+async function checkPublish(path: string): Promise<boolean | undefined> {
+  try {
+    const contents: FrontMatterResult<{publish?: boolean}> = fm(await fs.readFile(path, {encoding: "utf8"}));
+    if ("publish" in contents.attributes && typeof contents.attributes.publish === "boolean") {
+      return contents.attributes.publish;
+    }
+    return true;
+  } catch (err) {
+    console.error("failed to get frontmatter from", path);
+    console.error(err);
+  }
+}
+
+const allmdfiles = await glob('./vault/**/*.md',);
+const mdfiles = (await Promise.all(allmdfiles.map(async (filePath) => {
+  const result = await checkPublish(filePath);
+  return result === true ? filePath : false;
+}))).filter(filePath => filePath !== false);
+
 const baseUrl = "swtcpedia/"
 
 const groupItems = (filePaths: string[], target: string) => {
@@ -11,6 +37,7 @@ const groupItems = (filePaths: string[], target: string) => {
     file: path.basename(filePath, ".md")
       .toLowerCase()
       .replaceAll(" ", "-")
+      .replaceAll(".", "")
       .replace(/\(|\)|,/gi, ""), 
     name: path.basename(filePath, ".md")
   }))
@@ -21,11 +48,16 @@ const groupItems = (filePaths: string[], target: string) => {
   .sort(sortGroupFunc)
 }
 
-const findModules = (filePaths: string[]) => {
-  return filePaths.filter(filePath => 
-    filePath.toLowerCase().includes("modules")
-    &&
-    path.dirname(filePath).split(path.sep).pop() === path.basename(filePath, ".md"))
+const findFolders = (filePaths: string[], target: string) => {
+  const folderNames = new Set(
+    filePaths.filter(filePath => filePath.toLowerCase().includes(target))
+    .map(filePath => path.dirname(filePath).split(path.sep).pop())
+    
+  )
+  return Array.from(folderNames).sort().map(name => {
+    return {label: name, collapsed: false, items: groupItems(mdfiles, name!.toLowerCase())}
+  })
+
 }
 
 type GroupItem = {
@@ -41,10 +73,13 @@ const sortGroupFunc = (a: GroupItem, b: GroupItem) => {
   return 0;
 }
 
+
 export const groupedItems = [
-  {label: "Concepts", collapsed: true, items: groupItems(mdfiles, "concepts")},
-  {label: "Modules", collapsed: true, items: groupItems(findModules(mdfiles), "modules")}, // need to update this for the new structure
-  {label: "All Roles", collapsed: true, items: groupItems(mdfiles, "roles")},
-  {label: "All Characteristics", collapsed: true, items: groupItems(mdfiles, "characteristics")},
-  {label: "All Locations", collapsed: true, items: groupItems(mdfiles, "locations")},
+  {label: "Glossary", collapsed: true, items: groupItems(mdfiles, "glossary")},
+  {label: "Tutorial", collapsed: true, items: findFolders(mdfiles, "tutorial")},
+  // {label: "Modules", collapsed: true, items: groupItems(findModules(mdfiles), "modules")}, // need to update this for the new structure
+  {label: "Scenarios", collapsed: true, items: groupItems(mdfiles, "scenarios")},
+  {label: "Roles", collapsed: true, items: groupItems(mdfiles, "roles")},
+  {label: "Characteristics", collapsed: true, items: groupItems(mdfiles, "characteristics")},
+  {label: "Locations", collapsed: true, items: groupItems(mdfiles, "locations")},
 ]
